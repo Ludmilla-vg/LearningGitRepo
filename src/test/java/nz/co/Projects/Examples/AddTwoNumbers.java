4ceb03316db7f7a74f69dfb1acd2ed23d640125c
ludmilla <lchellemben@gmail.com>
2021-04-11 14:40:52 +1200

    replace the * by +

--- src/test/java/nz/co/Projects/Examples/AddTwoNumbers.java
+++ src/test/java/nz/co/Projects/Examples/AddTwoNumbers.java
@@ -4,7 +4,7 @@ public class AddTwoNumbers {
     public static void main(String [] args){
         int x =10;
         int y =20;
-        int z = x*y;
+        int z = x+y;
         System.out.println("The sum of x and y is: " + z);
     }
 
