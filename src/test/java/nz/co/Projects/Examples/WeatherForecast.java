63f828079a5379ab82cf02444c80b0f7cc408f92
ludmilla <lchellemben@gmail.com>
2021-04-11 15:55:34 +1200

    sdfsdfsdf

---
+++ src/test/java/nz/co/Projects/Examples/WeatherForecast.java
@@ -0,0 +1,7 @@
+package nz.co.Projects.Examples;
+
+public class WeatherForecast {
+    public static void main(String [] args){
+        System.out.println("No weather prediction this week.");
+    }
+}
