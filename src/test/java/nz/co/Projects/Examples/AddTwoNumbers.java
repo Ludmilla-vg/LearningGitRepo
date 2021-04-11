3a29b6acdc10d4cdd28c0a303d168e32eecb7814
ludmilla <lchellemben@gmail.com>
2021-04-11 14:37:30 +1200

    sum of 2 numbers

---
+++ src/test/java/nz/co/Projects/Examples/AddTwoNumbers.java
@@ -0,0 +1,11 @@
+package nz.co.Projects.Examples;
+
+public class AddTwoNumbers {
+    public static void main(String [] args){
+        int x =10;
+        int y =20;
+        int z = x*y;
+        System.out.println("The sum of x and y is: " + z);
+    }
+
+}
