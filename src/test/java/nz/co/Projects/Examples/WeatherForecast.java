964b061e98def9d2a2479f3ba11fdb505148c581
ludmilla <lchellemben@gmail.com>
2021-04-11 15:52:04 +1200

    Add weather forecast branch

---
+++ src/test/java/nz/co/Projects/Examples/WeatherForecast.java
@@ -0,0 +1,7 @@
+package nz.co.Projects.Examples;
+
+public class WeatherForecast {
+    public static void main (String [] args){
+        System.out.println("We will have a good weather this weekend");
+    }
+}
