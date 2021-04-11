db35f5b6941bf38b9b4c52e7d6cedebe1b2a7075
ludmilla <lgoornaden@gmail.com>
2021-04-11 21:55:46 +1200

    Add the NZ flag colour

--- src/test/java/nz/co/Projects/Examples/ListOfColours.java
+++ src/test/java/nz/co/Projects/Examples/ListOfColours.java
@@ -6,12 +6,25 @@ import java.util.List;
 public class ListOfColours {
     public static void main(String[] args) {
         List<String> listOfColours = new ArrayList<>();
+        List<String> listOfBabiesColours = new ArrayList<>();
+        List<String> listOfNZFlag = new ArrayList<>();
+        System.out.println("The mauritian flag: ");
         listOfColours.add("Red");
         listOfColours.add("Blue");
         listOfColours.add("Yellow");
         listOfColours.add("Green");
 
+        System.out.println("Some babies colours are: ");
+        listOfBabiesColours.add("pink");
+        listOfBabiesColours.add("blue");
+        listOfBabiesColours.add("white");
+        listOfBabiesColours.add("purple");
         System.out.println(listOfColours);
+
+        System.out.println("The New Zealand Flag: ");
+        listOfNZFlag.add("WHITE");
+        listOfNZFlag.add("RED");
+        listOfNZFlag.add("BLUE");
     }
 }
 
