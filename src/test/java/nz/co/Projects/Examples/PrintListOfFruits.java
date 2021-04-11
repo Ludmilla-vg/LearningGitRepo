75c8f142552d81e451078d9d9b160fd146b357eb
ludmilla <lchellemben@gmail.com>
2021-04-11 15:55:34 +1200

    sdfsdfsdf

--- src/test/java/nz/co/Projects/Examples/PrintListOfFruits.java
+++ src/test/java/nz/co/Projects/Examples/PrintListOfFruits.java
@@ -4,15 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrintListOfFruits {
-    public static class PrintLisfOfFruits {
-        public static void main (String[] args){
-            System.out.println("The list of fruits are: ");
-            List<String> listOfFruits = new ArrayList<>();
-            listOfFruits.add("Apple");
-            listOfFruits.add("Banana");
-            listOfFruits.add("Cherry");
-            listOfFruits.add("Dragon Fruit");
-            System.out.println(listOfFruits);
-        }
+    public static void main(String[] args) {
+        System.out.println("The list of fruits are: ");
+        List<String> listOfFruits = new ArrayList<>();
+        listOfFruits.add("Apple");
+        listOfFruits.add("Banana");
+        listOfFruits.add("Cherry");
+        listOfFruits.add("Dragon Fruit");
+        System.out.println(listOfFruits);
     }
 }
