5d938a3151502370923948eb36af3618b6c50881
ludmilla <lchellemben@gmail.com>
2021-04-11 14:26:09 +1200

    List of colours

---
+++ src/test/java/nz/co/Projects/Examples/ListOfColours.java
@@ -0,0 +1,18 @@
+package nz.co.Projects.Examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfColours {
+    public static void main(String[] args) {
+        List<String> listOfColours = new ArrayList<>();
+        listOfColours.add("Red");
+        listOfColours.add("Blue");
+        listOfColours.add("Yellow");
+        listOfColours.add("Green");
+
+        System.out.println(listOfColours);
+    }
+}
+
+
