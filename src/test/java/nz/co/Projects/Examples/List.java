367f36d5166ccaba5382a4b43b1a2646ca6e44bd
ludmilla <lgoornaden@gmail.com>
2021-04-17 06:57:19 +1200

    no list

--- src/test/java/nz/co/Projects/Examples/List.java
+++ src/test/java/nz/co/Projects/Examples/List.java
@@ -3,5 +3,8 @@ package nz.co.Projects.Examples;
 public class List {
     public static void main (String [] args){
         System.out.println("Nothing in the list");
+        System.out.println("Are u sure there is no list");
+
+
     }
 }
