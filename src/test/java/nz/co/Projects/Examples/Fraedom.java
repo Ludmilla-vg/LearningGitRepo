fd9604c92084b8a444c88b3d5b9fbfa4f5487b06
ludmilla <lgoornaden@gmail.com>
2021-04-16 22:17:48 +1200

    Fraedom company

---
+++ src/test/java/nz/co/Projects/Examples/Fraedom.java
@@ -0,0 +1,8 @@
+package nz.co.Projects.Examples;
+
+public class Fraedom {
+    public static void main (String [] args){
+        System.out.println("Fraedom is a company that deals with corporate cards");
+    }
+}
+
