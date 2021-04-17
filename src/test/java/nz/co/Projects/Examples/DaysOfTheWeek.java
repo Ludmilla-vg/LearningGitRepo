a72131eee1e8d34cd3b74f378e598647c6835f75
ludmilla <lgoornaden@gmail.com>
2021-04-17 23:28:06 +1200

    add weekend

---
+++ src/test/java/nz/co/Projects/Examples/DaysOfTheWeek.java
@@ -0,0 +1,13 @@
+package nz.co.Projects.Examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DaysOfTheWeek {
+    public static void main(String[] args) {
+        List<String> weekdays = new ArrayList<>();
+        weekdays.add("Saturday");
+        weekdays.add("Sunday");
+    }
+}
+
