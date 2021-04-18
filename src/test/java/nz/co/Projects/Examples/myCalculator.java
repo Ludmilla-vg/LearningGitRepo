37b1750bf473851220b3ee7d984e034c6e61f85f
ludmilla <lgoornaden@gmail.com>
2021-04-18 16:13:36 +1200

    The sum and product of 2 numbers

---
+++ src/test/java/nz/co/Projects/Examples/myCalculator.java
@@ -0,0 +1,12 @@
+package nz.co.Projects.Examples;
+
+public class myCalculator {
+    public static void main(String [] args){
+        int value1 = 100;
+        int value2 = 20;
+        int sum = value1 + value2;
+        int subtract = value1-value2;
+        System.out.println("The sum of the 2 numbers is => "+sum);
+        System.out.println("The difference between the 2 numbers is => "+subtract);
+    }
+}
