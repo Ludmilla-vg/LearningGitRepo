4559716efdaf2b29104eb032d289192be3feafe1
ludmilla <lgoornaden@gmail.com>
2021-04-18 22:07:27 +1200

    Phase 2 of cherry picking

---
+++ src/test/java/nz/co/Projects/Examples/AnotherCherryPicking.java
@@ -0,0 +1,7 @@
+package nz.co.Projects.Examples;
+
+public class AnotherCherryPicking {
+    public static void main(String [] args){
+        System.out.println("Phase 2 of cherry picking");
+    }
+}
