9dff55a6a34a7ad5bd0f6b1261dd0dcd0d17e5de
ludmilla <lgoornaden@gmail.com>
2021-04-25 14:54:18 +1200

    list of months

---
+++ src/test/java/nz/co/Projects/Examples/ListOfMonths.java
@@ -0,0 +1,13 @@
+package nz.co.Projects.Examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfMonths {
+    public static void main(String[] args) {
+        List<String> months = new ArrayList<>();
+        months.add("October");
+        months.add("November");
+        months.add("December");
+    }
+}
